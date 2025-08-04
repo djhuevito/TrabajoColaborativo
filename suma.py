d2250af7bb8b8f26768caf332c61c20c56e0ee4e
pgranda71 <pgranda71@gmail.com>
2025-08-03 20:40:51 -0500

    Update suma.py
    Agregar descripcion

--- suma.py
+++ suma.py
@@ -1,6 +1,7 @@
-#ingresa númweros
+#ingresa números enteros
 numero1=int(input("Ingrese un numero: "))
 numero2=float(input("Ingrese un numero: "))
 #sumamos
 total=numero1 + numero2
+
 print ("La suma de",numero1, "y", numero2, "es",total)
