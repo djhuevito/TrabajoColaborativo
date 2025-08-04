94704ca4be91b9191bdf9c2b8ee8fe3ee55b6e38
pgranda71 <pgranda71@gmail.com>
2025-08-03 20:20:24 -0500

    Add files via upload
    Programa SUMA

---
+++ suma.py
@@ -0,0 +1,6 @@
+#ingresa númweros
+numero1=int(input("Ingrese un numero: "))
+numero2=float(input("Ingrese un numero: "))
+#sumamos
+total=numero1 + numero2
+print ("La suma de",numero1, "y", numero2, "es",total)
